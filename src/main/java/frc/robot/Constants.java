8b2dde1b718d0cfc688a6746a547d89267311eaf
Gabe <gabe.seq@gmail.com>
2023-01-23 18:35:05 -0500

    finalized brain surgery

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,19 +6,5 @@ package frc.robot;
 
 /** Add your docs here. */
 public class Constants {
-    public static final class DriveConstants {
-        public static final double SPEED_SCALE = 0.25;
-    }
-
-    public static final class PIDConstants {
-        public static final class Theta { // TODO: put in drivetrains
-            public static final double kP = 6.3;
-            public static final double kD = 0.05;
-            public static final double[] gains = { kP, 0, kD };
-        }
-    }
-
-    public static final class OIConstants {
-        public static final int DRIVER = 0;
-    }
+    
 }
